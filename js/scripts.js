001aa07d46ee48cd5ac211237d69aac235d2001b
Karan <ksharma@conestogac.on.ca>
2022-06-11 23:56:58 -0400

    Contact Page Updates

--- js/scripts.js
+++ js/scripts.js
@@ -19,20 +19,20 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
-    /*const mainNav = document.body.querySelector('#mainNav');
+    const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new body.ScrollSpy(document.body, {
             target: '#mainNav',
             offset: 74,
         });
-    };*/
+    };
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
